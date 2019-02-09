ddfee0bc3d1c8d6381d387b3bbd34a4625a45c95
bedoyama <bedoyama@gmail.com>
2019-02-09 14:53:49 -0800

    added link to book

--- app/views/book-list/book-list.js
+++ app/views/book-list/book-list.js
@@ -10,6 +10,27 @@ angular.module('myApp.bookList', ['ngRoute'])
     }])
 
     .controller('BookListCtrl', ['$scope', 'restService', function ($scope, restService) {
+        $scope.gridOptions = {
+            columnDefs: [
+                {
+                    name: 'title',
+                    cellTemplate: '<div class="ui-grid-cell-contents"><a href="{{COL_FIELD.link}}">{{ COL_FIELD.name }}</a> </div>'
+                },
+                {
+                    name: 'year'
+                },
+                {
+                    name: 'publisher'
+                },
+                {
+                    name: 'description'
+                },
+                {
+                    name: 'language'
+                }
+            ]
+        };
+
         function getBooks() {
             restService.getBooks()
                 .then(function (response) {
@@ -17,15 +38,20 @@ angular.module('myApp.bookList', ['ngRoute'])
                     $scope.wrapper = $scope.response['_embedded'];
                     $scope.books = $scope.wrapper.book;
 
-                    $scope.books = _.map($scope.books, function (book) {
+                    $scope.gridOptions.data = _.map($scope.books, function (book) {
                         return {
-                            title: book.title,
+                            title: {
+                                name: book.title,
+                                link: book._links.self.href
+                            },
                             year: book.year,
                             publisher: book.publisher,
                             description: book.description,
                             language: book.language
                         };
                     });
+
+                    console.log($scope.gridOptions.data);
                 }, function (error) {
                     $scope.status = 'Unable to load customer data: ' + error.message;
                 });
